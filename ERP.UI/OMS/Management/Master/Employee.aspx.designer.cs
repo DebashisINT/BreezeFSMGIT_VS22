95591056d14c4abae84ef1083917acc9344c04bf
PritiINT <62432969+PritiINT@users.noreply.github.com>
2023-02-20 16:48:54 +0530

    CHECK IN

--- ERP.UI/OMS/Management/Master/Employee.aspx.designer.cs
+++ ERP.UI/OMS/Management/Master/Employee.aspx.designer.cs
@@ -436,5 +436,14 @@ namespace ERP.OMS.Management.Master
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button BtnSaveexcel;
+
+        /// <summary>
+        /// GvImportDetailsSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxGridView GvImportDetailsSearch;
     }
 }
