7ffaacbbabf9d59447280b0aa44ece814d8c34cd
Sanchita Saha <sanchita.saha@indusnet.co.in>
2024-06-11 12:07:56 +0530

    commit

---
+++ LMS/Areas/LMS/Controllers/TopicsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Areas.LMS.Controllers
+{
+    public class TopicsController : Controller
+    {
+        // GET: LMS/Topics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
