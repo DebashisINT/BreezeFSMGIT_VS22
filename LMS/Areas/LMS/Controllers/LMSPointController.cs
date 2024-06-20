266999a9e813fe86bbc3663e2bdec089e0cb3758
PritiINT <62432969+PritiINT@users.noreply.github.com>
2024-06-20 17:36:56 +0530

    check in

---
+++ LMS/Areas/LMS/Controllers/LMSPointController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Areas.LMS.Controllers
+{
+    public class LMSPointController : Controller
+    {
+        // GET: LMS/LMSPoint
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
