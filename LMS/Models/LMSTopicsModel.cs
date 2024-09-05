972be67ba4969ab72f2d4cd9863425adf75febf4
Sanchita Saha <sanchita.saha@indusnet.co.in>
2024-09-05 13:28:06 +0530

    commit

--- LMS/Models/LMSTopicsModel.cs
+++ LMS/Models/LMSTopicsModel.cs
@@ -53,4 +53,83 @@ namespace LMS.Models
 
     }
 
+    public class Data
+    {
+
+        public string body
+        {
+            get;
+            set;
+        }
+
+        public string title
+        {
+            get;
+            set;
+        }
+
+        public string key_1
+        {
+            get;
+            set;
+        }
+
+        public string key_2
+        {
+            get;
+            set;
+        }
+
+    }
+
+    public class Message
+    {
+
+        public string token
+        {
+            get;
+            set;
+        }
+
+        public Data data
+        {
+            get;
+            set;
+        }
+
+        public Notification notification
+        {
+            get;
+            set;
+        }
+
+    }
+
+    public class Notification
+    {
+
+        public string title
+        {
+            get;
+            set;
+        }
+
+        public string body
+        {
+            get;
+            set;
+        }
+
+    }
+
+    public class Root
+    {
+
+        public Message message
+        {
+            get;
+            set;
+        }
+
+    }
 }
