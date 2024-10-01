307c8f54b490a5713f4f2970eeb3483a888a18b0
Debashis Talukder <debashis.talukder@indusnet.co.in>
2024-10-01 17:41:43 +0530

    Commit
    Done

--- ShopAPI/Controllers/OrderWithStockMgmtDetailsController.cs
+++ ShopAPI/Controllers/OrderWithStockMgmtDetailsController.cs
@@ -1,6 +1,6 @@
 ﻿#region======================================Revision History=========================================================
 //Written By : Debashis Talukder On 30/09/2024
-//Purpose: Product Stock Management Details.Refer: Row: 982
+//Purpose: Product Stock Management Details.Refer: Row: 982 & 984
 #endregion===================================End of Revision History==================================================
 
 using Newtonsoft.Json;
@@ -13,6 +13,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Xml.Linq;
 
 namespace ShopAPI.Controllers
 {
@@ -52,6 +53,64 @@ namespace ShopAPI.Controllers
                         omodel.message = "Successfully Get List.";
                         Soview = APIHelperMethods.ToModelList<ProductStocklistOutput>(dt);
                         omodel.stock_list = Soview;
+                    }
+                    else
+                    {
+                        omodel.status = "205";
+                        omodel.message = "No data found";
+                    }
+                    var message = Request.CreateResponse(HttpStatusCode.OK, omodel);
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                omodel.status = "204";
+                omodel.message = ex.Message;
+                var message = Request.CreateResponse(HttpStatusCode.OK, omodel);
+                return message;
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage UpdateProductBalStock(UpdateProductBalStockInput model)
+        {
+            UpdateProductBalStockOutput omodel = new UpdateProductBalStockOutput();
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    omodel.status = "213";
+                    omodel.message = "Some input parameters are missing.";
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, omodel);
+                }
+                else
+                {
+                    DataTable dt = new DataTable();
+                    String con = System.Configuration.ConfigurationManager.AppSettings["DBConnectionDefault"];
+                    SqlCommand sqlcmd = new SqlCommand();
+                    SqlConnection sqlcon = new SqlConnection(con);
+                    sqlcon.Open();
+                    sqlcmd = new SqlCommand("PRC_FSMSTOCKMGMTINFODETAILS", sqlcon);
+                    sqlcmd.Parameters.AddWithValue("@ACTION", "UPDATEPRODBALSTOCK");
+                    sqlcmd.Parameters.AddWithValue("@USER_ID", model.user_id);
+                    sqlcmd.Parameters.AddWithValue("@STOCK_SHOPCODE", model.stock_shopcode);
+                    sqlcmd.Parameters.AddWithValue("@STOCK_PRODUCTID", model.stock_productid);
+                    sqlcmd.Parameters.AddWithValue("@SUBMITTED_QTY", model.submitted_qty);
+
+                    sqlcmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+                    da.Fill(dt);
+                    sqlcon.Close();
+                    if (dt.Rows.Count > 0)
+                    {
+                        omodel.status = "200";
+                        omodel.message = "Data Updated Successfully.";
+                    }
+                    else
+                    {
+                        omodel.status = "202";
+                        omodel.message = "No entry.";
                     }
                     var message = Request.CreateResponse(HttpStatusCode.OK, omodel);
                     return message;
--- ShopAPI/Models/OrderWithStockMgmtDetailsModel.cs
+++ ShopAPI/Models/OrderWithStockMgmtDetailsModel.cs
@@ -1,6 +1,6 @@
 ﻿#region======================================Revision History=========================================================
 //Written By : Debashis Talukder On 30/09/2024
-//Purpose: Product Stock Management Details.Refer: Row: 982
+//Purpose: Product Stock Management Details.Refer: Row: 982 & 984
 #endregion===================================End of Revision History==================================================
 
 using System;
@@ -30,5 +30,18 @@ namespace ShopAPI.Models
         public string stock_productname { get; set; }
         public decimal stock_productqty { get; set; }
         public decimal stock_productbalqty { get; set; }
+    }
+
+    public class UpdateProductBalStockInput
+    {
+        public long user_id { get; set; }
+        public string stock_shopcode { get; set; }
+        public string stock_productid { get; set; }
+        public decimal submitted_qty { get; set; }
+    }
+    public class UpdateProductBalStockOutput
+    {
+        public string status { get; set; }
+        public string message { get; set; }
     }
 }
