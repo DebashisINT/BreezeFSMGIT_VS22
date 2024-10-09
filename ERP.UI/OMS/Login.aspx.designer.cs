17f8c2d05446ebcf3c7c64ae655f519eb137cf63
PallabMukherjeeINT <pallab.mukherjee@indusnet.co.in>
2024-10-09 18:22:47 +0530

    commit
    commit

--- ERP.UI/OMS/Login.aspx.designer.cs
+++ ERP.UI/OMS/Login.aspx.designer.cs
@@ -13,6 +13,15 @@ public partial class pLogin
 {
 
     /// <summary>
+    /// lnlDownloaderexcel control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.LinkButton lnlDownloaderexcel;
+
+    /// <summary>
     /// EV1 control.
     /// </summary>
     /// <remarks>
@@ -92,15 +101,6 @@ public partial class pLogin
     /// To modify move field declaration from designer file to code-behind file.
     /// </remarks>
     protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
-
-    /// <summary>
-    /// lnlDownloaderexcel control.
-    /// </summary>
-    /// <remarks>
-    /// Auto-generated field.
-    /// To modify move field declaration from designer file to code-behind file.
-    /// </remarks>
-    protected global::System.Web.UI.WebControls.LinkButton lnlDownloaderexcel;
 
     /// <summary>
     /// lblVersion control.
