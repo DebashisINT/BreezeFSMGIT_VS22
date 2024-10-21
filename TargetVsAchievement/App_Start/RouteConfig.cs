edba28b60f9a6bf10efc4c1b04498e0d8deee622
Sanchita Saha <sanchita.saha@indusnet.co.in>
2024-10-21 12:30:35 +0530

    commit

--- TargetVsAchievement/App_Start/RouteConfig.cs
+++ TargetVsAchievement/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace TargetVsAchievement
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional },
+                new string[] { "TargetVsAchievement.Controllers" }
             );
         }
     }
