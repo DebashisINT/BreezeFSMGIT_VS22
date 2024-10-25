ef43759d6cc9bf126b9cb2111fdb5b97ff47c42a
PritiINT <62432969+PritiINT@users.noreply.github.com>
2024-10-25 12:34:39 +0530

    check in

---
+++ TargetVsAchievement/Models/SalesTargetModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using DataAccessLayer;
+using DevExpress.Web;
+using DevExpress.Web.Mvc;
+namespace TargetVsAchievement.Models
+{
+    public class SalesTargetModel
+    {
+        public Int64 SALESTARGET_ID { get; set; }
+        public String SalesTargetLevel { get; set; }
+        public String SalesTargetNo { get; set; }
+        public String SalesTargetDate { get; set; }
+       // public List<SalesTargetProduct> ListSalesTargetProduct { get; set; }
+        public DataSet SalesTargetEntryInsertUpdate(String action, DateTime? SalesTargetDate, Int64 SALESTARGET_ID, String SalesTargetLevel,String SalesTargetNo,
+            DataTable dtSalesTarget,Int64 userid = 0
+            )
+        {
+            DataSet ds = new DataSet();           
+            ProcedureExecute proc = new ProcedureExecute("usp_BOMProductEntryGetNew");
+            //End of rev Pratik
+            proc.AddVarcharPara("@ACTION", 150, action);
+            proc.AddVarcharPara("@BOMNO", 100, SalesTargetLevel);
+            proc.AddDateTimePara("@SalesTargetDate", Convert.ToDateTime(SalesTargetDate));
+            proc.AddBigIntegerPara("@SALESTARGET_ID", SALESTARGET_ID);           
+            proc.AddVarcharPara("@FINISHEDUOM", 100, SalesTargetNo);         
+           
+            if (action == "INSERTMAINPRODUCT" || action == "UPDATEMAINPRODUCT")
+            {
+                proc.AddPara("@UDTSalesTarget", dtSalesTarget);
+            }           
+            ds = proc.GetDataSet();
+            return ds;
+        }
+
+    }
+    public class SalesTargetProduct
+    {
+        public String ActualSL { get; set; }
+        public string SlNO { get; set; }
+        //public Int64 SALESTARGETDETAILS_ID { get; set; }
+        
+
+        public string TARGETDOCNUMBER { get; set; }
+
+        public string TARGETLEVELID { get; set; }
+
+        public string TARGETLEVEL { get; set; }
+
+        //public string INTERNALID { get; set; }
+
+
+        public string TIMEFRAME { get; set; }
+
+        public DateTime STARTEDATE { get; set; }
+
+        public DateTime ENDDATE { get; set; }
+
+        public Int64 NEWVISIT { get; set; }
+
+        public Int64 REVISIT { get; set; }
+
+        public decimal ORDERAMOUNT { get; set; }
+
+        public decimal COLLECTION { get; set; }
+
+        public decimal ORDERQTY { get; set; }
+
+        public string UpdateEdit { get; set; }
+        
+
+    }
+
+    public class udtSalesTarget
+    {
+        public string SlNO { get; set; }
+        public Int64 SALESTARGETDETAILS_ID { get; set; }
+
+
+
+        public Int64 TARGETLEVELID { get; set; }
+
+
+
+        public string INTERNALID { get; set; }
+
+
+        public string TIMEFRAME { get; set; }
+
+        public DateTime STARTEDATE { get; set; }
+
+        public DateTime ENDDATE { get; set; }
+
+        public Int64 NEWVISIT { get; set; }
+
+        public Int64 REVISIT { get; set; }
+
+        public decimal ORDERAMOUNT { get; set; }
+
+        public decimal COLLECTION { get; set; }
+
+        public decimal ORDERQTY { get; set; }
+
+        public Int64 UpdateEdit { get; set; }
+    }
+
+
+
+   
+}
---
+++ TargetVsAchievement/Models/TargetWebService.asmx.cs
@@ -0,0 +1,89 @@
+﻿using DataAccessLayer;
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace TargetVsAchievement.Models
+{
+    /// <summary>
+    /// Summary description for TargetWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+     [System.Web.Script.Services.ScriptService]
+    public class TargetWebService : System.Web.Services.WebService
+    {
+
+        string ConnectionString = String.Empty;
+        public TargetWebService()
+        {
+            ConnectionString = Convert.ToString(System.Web.HttpContext.Current.Session["DBConnectionDefault"]);
+        }
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod(EnableSession = true)]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public object GetTargetLevelDetailsList(string SearchKey, string Type)
+        {
+            
+            List<SalesTargetLevelDetails> list = new List<SalesTargetLevelDetails>();
+           
+            if (HttpContext.Current.Session["userid"] != null)
+            {
+                SearchKey = SearchKey.Replace("'", "''");
+                BusinessLogicLayer.DBEngine oDBEngine = new BusinessLogicLayer.DBEngine(ConnectionString);
+                string Query = "";
+                Query = @"   EXEC PRC_FSMSALESTARGET '" + Type + "','" + SearchKey + "' ";
+
+
+                DataTable dt = oDBEngine.GetDataTable(Query);
+
+                if (!String.IsNullOrEmpty(Type))
+                {
+
+                    list = (from DataRow dr in dt.Rows
+                            select new SalesTargetLevelDetails()
+                            {
+                                Level_ID = Convert.ToString(dr["ID"]),
+                                Level_Code = Convert.ToString(dr["CODE"]),
+                                Level_Name = Convert.ToString(dr["NAME"]),
+
+                            }).ToList();
+                }
+                else
+                {
+                    list = (from DataRow dr in dt.Rows
+                            select new SalesTargetLevelDetails()
+                            {
+                                Level_ID = Convert.ToString(dr["ID"]),
+                                Level_Code = Convert.ToString(dr["CODE"]),
+                                Level_Name = Convert.ToString(dr["NAME"]),
+                               
+                            }).ToList();
+                }
+
+            }
+
+            return list;
+        }
+    }
+
+    public class SalesTargetLevelDetails
+    {
+        public string Level_ID { get; set; }
+        public string Level_Code { get; set; }
+        public string Level_Name { get; set;}
+    }
+}
