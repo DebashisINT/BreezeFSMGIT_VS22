7c5415d2887e67fb98c468df6b876447d1b696fb
PritiINT <62432969+PritiINT@users.noreply.github.com>
2024-11-04 18:51:00 +0530

    check in

---
+++ TargetVsAchievement/Areas/TargetVsAchievement/Controllers/BrandVolumeValueTargetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TargetVsAchievement.Areas.TargetVsAchievement.Controllers
+{
+    public class BrandVolumeValueTargetController : Controller
+    {
+        // GET: TargetVsAchievement/BrandVolumeValueTarget
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
